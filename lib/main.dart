8394ebe78a79b241cf1a16aa0df20de5532bcf6b
charan <shhrikatta@gmail.com>
2022-04-06 09:19:51 +0530

    UI module

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dapp_wallet/widgets/slider.dart';
 import 'package:http/http.dart';
+import 'package:velocity_x/velocity_x.dart';
 import 'package:web3dart/web3dart.dart';
 
 void main() {
@@ -52,6 +54,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   late Client httpClient;
   late Web3Client web3client;
+  bool data = false;
+  double myAmount = 0;
 
   @override
   void initState() {
@@ -67,12 +71,79 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: const Center(),
+      backgroundColor: Vx.gray300,
+      body: ZStack([
+        VxBox()
+            .blue600
+            .size(context.screenWidth, context.percentHeight * 30)
+            .make(),
+        VStack([
+          (context.percentHeight * 10).heightBox,
+          widget.title.text.xl4.white.bold.center.makeCentered().py16(),
+          (context.percentHeight * 5).heightBox,
+          VxBox(
+            child: VStack([
+              "Balance".text.gray700.xl2.semiBold.makeCentered(),
+              10.heightBox,
+              data
+                  ? "\$1".text.bold.xl6.makeCentered()
+                  : CircularProgressIndicator().centered()
+            ]),
+          )
+              .p16
+              .white
+              .size(context.screenWidth, context.percentHeight * 18)
+              .rounded
+              .shadowXl
+              .make()
+              .p16(),
+          30.heightBox,
+          SliderWidget(
+            min: 0,
+            max: 100,
+            finalValue: (value) {
+              myAmount = (value * 100);
+              print(myAmount);
+            },
+          ).centered(),
+          HStack(
+            [
+              FlatButton.icon(
+                onPressed: () {},
+                color: Colors.blue,
+                shape: Vx.roundedSm,
+                icon: const Icon(
+                  Icons.refresh,
+                  color: Colors.white,
+                ),
+                label: "Refresh".text.white.make(),
+              ).h(50),
+              FlatButton.icon(
+                onPressed: () {},
+                color: Colors.green,
+                shape: Vx.roundedSm,
+                icon: const Icon(
+                  Icons.call_made_outlined,
+                  color: Colors.white,
+                ),
+                label: "Deposit".text.white.make(),
+              ).h(50),
+              FlatButton.icon(
+                onPressed: () {},
+                color: Colors.red,
+                shape: Vx.roundedSm,
+                icon: const Icon(
+                  Icons.call_received_outlined,
+                  color: Colors.white,
+                ),
+                label: "Withdraw".text.white.make(),
+              ).h(50),
+            ],
+            alignment: MainAxisAlignment.spaceAround,
+            axisSize: MainAxisSize.max,
+          ).p16(),
+        ])
+      ]),
     );
   }
 }
---
+++ lib/widgets/slider.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+import 'custom_slider_thumb_circle.dart';
+
+class SliderWidget extends StatefulWidget {
+  final double sliderHeight;
+  final int min;
+  final int max;
+  final fullWidth;
+  final ValueChanged<double> finalValue;
+
+  const SliderWidget(
+      {Key? key,
+      this.sliderHeight = 48,
+      this.max = 10,
+      this.min = 0,
+      this.fullWidth = false,
+      required this.finalValue})
+      : super(key: key);
+
+  @override
+  _SliderWidgetState createState() => _SliderWidgetState();
+}
+
+class _SliderWidgetState extends State<SliderWidget> {
+  double _value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    double paddingFactor = .2;
+
+    if (widget.fullWidth) paddingFactor = .3;
+
+    return Container(
+      width: widget.fullWidth ? double.infinity : (widget.sliderHeight) * 5.5,
+      height: (widget.sliderHeight),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular((widget.sliderHeight * .3)),
+        ),
+        gradient: const LinearGradient(
+            colors: [
+              Color(0xFF00c6ff),
+              Color(0xFF0072ff),
+            ],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 1.00),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(widget.sliderHeight * paddingFactor, 2,
+            widget.sliderHeight * paddingFactor, 2),
+        child: Row(
+          children: <Widget>[
+            Text(
+              '${widget.min}',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: widget.sliderHeight * .3,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              width: widget.sliderHeight * .1,
+            ),
+            Expanded(
+              child: Center(
+                child: SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    activeTrackColor: Colors.white.withOpacity(1),
+                    inactiveTrackColor: Colors.white.withOpacity(.5),
+
+                    trackHeight: 4.0,
+                    thumbShape: CustomSliderThumbCircle(
+                      thumbRadius: widget.sliderHeight * .4,
+                      min: widget.min,
+                      max: widget.max,
+                    ),
+                    overlayColor: Colors.white.withOpacity(.4),
+                    //valueIndicatorColor: Colors.white,
+                    activeTickMarkColor: Colors.white,
+                    inactiveTickMarkColor: Colors.red.withOpacity(.7),
+                  ),
+                  child: Slider(
+                      value: _value,
+                      onChanged: (value) {
+                        setState(() {
+                          _value = value;
+                        });
+                      }),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: widget.sliderHeight * .1,
+            ),
+            Text(
+              '${widget.max}',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: widget.sliderHeight * .3,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
